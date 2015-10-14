24ba7ad74e6942d6f218d91b41e46fe51ad62d4a
tkmagesh <tkmagesh77@gmail.com>
2015-10-14 11:28:47 +0530

    Server Sent Events

--- Day-02/04-WebWorker-Cont/myWorker.js
+++ Day-02/04-WebWorker-Cont/myWorker.js
@@ -1,15 +1,23 @@
 console.log("Worker loaded");
 function doWork(){
-    for(var i=0; i< (10000); i++)
+    for(var i=0; i< (10000); i++){
         for(var j=0; j<10000; j++)
             for(var k=0; k<100; k++){
             }
+        if ((i+1) % 100 === 0){
+            var percentCompleted = ((i+1) / 10000) * 100;
+            self.postMessage({
+                type : 'progress',
+                percentCompleted : percentCompleted
+            });
+        }
+    }
     console.log("do work - job done");
 }
 self.addEventListener("message", function(evtArg){
     if (evtArg.data === 'start'){
         doWork();
-        self.postMessage("done");
+        self.postMessage({type : "done"});
     } else {
         console.log("unknown message - ", evtArg.data);
     }
--- Readme.txt
+++ Readme.txt
@@ -1,10 +1,10 @@
-1. Data Storage
+1. Data Storage (done)
     a.localStorage
     b.sessionStorage
     c.indexedDb
     d.websql (obsolete)
-2. Offline application cache
-3. Web Workers (multithreading)
+2. Offline application cache (done)
+3. Web Workers (multithreading) (done)
 4. Real Time Updates
     a. Server Sent Events
     b. Web Sockets
